250861d793dccf9816236a9bcc656712fbff24ea
준형 배 <bjh6681@outlook.kr>
2018-05-20 16:57:44 +0900

    크기비교연산자 수정

--- MiracleBoys/MiracleBoys_SDL/RenderableObject.h
+++ MiracleBoys/MiracleBoys_SDL/RenderableObject.h
@@ -10,15 +10,15 @@ protected:
 	inline void SetClipRect(SDL_Rect rct) { clipRect = rct; }
 	inline void SetClipRect(SDL_Point pt1, SDL_Point pt2)
 	{
-		pt1.x < pt2.x ? Swap(pt1, pt2) : NULL;
-		pt1.y < pt2.y ? Swap(pt1.y, pt2.y) : NULL;
+		pt1.x > pt2.x ? Swap(pt1, pt2) : NULL;
+		pt1.y > pt2.y ? Swap(pt1.y, pt2.y) : NULL;
 		SDL_Rect temp = { pt1.x,pt1.y,pt1.x - pt2.x,pt1.y - pt2.y };
 		SetClipRect(temp);
 	}
 	inline void SetClipRect(int x1, int y1, int x2, int y2)
 	{
-		x1 < x2 ? Swap(x1, x2) : NULL;
-		y1 < y2 ? Swap(y1, y2) : NULL;
+		x1 > x2 ? Swap(x1, x2) : NULL;
+		y1 > y2 ? Swap(y1, y2) : NULL;
 		SDL_Rect temp = { x1,y1,x1 - x2,y1 - y2 };
 		SetClipRect(temp);
 	}
