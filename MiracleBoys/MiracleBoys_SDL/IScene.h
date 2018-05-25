0d2025258d40413eb8329f5a44f80acc1bdac6fe
준형 배 <bjh6681@outlook.kr>
2018-05-26 00:02:34 +0900

    씬 인터페이스 멤버 추가 및 메서드 인자 추가

--- MiracleBoys/MiracleBoys_SDL/IScene.h
+++ MiracleBoys/MiracleBoys_SDL/IScene.h
@@ -1,11 +1,15 @@
 #pragma once
+class CSystem;
 
 class IScene
 {
+protected:
+	CSystem* perent;
+
 public:
 	virtual void OnChangeScene() {};
 
-	virtual void Init(SDL_Renderer*) = 0;
+	virtual void Init(CSystem* perent, SDL_Renderer*) = 0;
 	virtual void Update() = 0;
 	virtual void Render(SDL_Renderer*) = 0;
 	virtual void Destroy() = 0;
